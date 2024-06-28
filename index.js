e4e386b22195397b14606e66f306243ec31618ca
plug_keith <keith.mwai@student.moringaschool.com>
2024-06-28 20:34:47 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Eugene',
+    role: 'Security Analyst',
+    age: 20
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // update and return employee object
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    //clone of the employee object
+    let clonedEmployee = { ...employee };
+    // Delete the specified key and return
+    delete clonedEmployee[key];
+    return clonedEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Delete the specified key and return
+    delete employee[key];
+    return employee;
+}
